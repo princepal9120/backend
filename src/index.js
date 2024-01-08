5d9ed4f99050f453b2f3d4d302e6a14aa7a8c3e8
princepal9120 <princepal9120@gmail.com>
2024-01-09 00:38:10 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ dotenv.config({
   path: "./.env",
 });
 
-// connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 /*
 import express from "express"
