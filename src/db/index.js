1da23f5dafe272e9684e186317767e6fc94dfa6e
princepal9120 <princepal9120@gmail.com>
2024-01-08 11:27:11 +0530

    connect database to mern

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+const connectDB =async ()=>{
+    try
+    {
+       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`\n Mongodb connected  !! DB Host:${connectionInstance.connection.host}`);
+        
+    }catch(error){
+        console.log("Mongodb connection error  ",error)
+        process.exit(1)
+    }
+}
+export default connectDB
