4af90dfdc264686e85cce264c215d6aa4d1042c9
princepal9120 <princepal9120@gmail.com>
2024-01-10 23:39:27 +0530

    cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary } from "cloudinary"
+import { response } from "express";
+import fs from "fs"
+
+          
+cloudinary.config({ 
+  cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME', 
+  api_key: 'process.env.CLOUDINARY_API_KEY', 
+  api_secret: 'process.env.CLOUDINARY_API_SECRET' 
+});
+const uploadOnCloundinary= async (localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        // upload the file on cloudinary
+        cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        console.log("file is uploaded successfully", response.url);
+return response;
+    }catch(error){
+fs.unlinkSync(localFilePath)
+// remove the localy temporary file as the upload operation failed
+    }
+}
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
